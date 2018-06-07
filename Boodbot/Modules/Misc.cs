d882b0392a36a979b3e2bfb4cdd17df7985fc647
Alvin WaterAlvinsgre <xelkovaisbad@gmail.comger>
2018-06-06 21:13:24 -0400

    An awful new bot. Yes, I wrote that as awful, new.

---
+++ Boodbot/Modules/Misc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Boodbot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")] //echos text back
+        public async Task Echo([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Echo'd message");
+            embed.WithDescription(message);
+            embed.WithColor(new Color(0, 255, 0));
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+        }
+
+        [Command("Hello")] //still working on this command
+        public async Task Hello([Remainder]string message)
+        {
+            await Context.Channel.SendMessageAsync("@" + Context.User + " You suck.");
+        }
+    }
+}
--- Boodbot/Program.cs
+++ Boodbot/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 
 namespace Boodbot
 {
     class Program
     {
+        DiscordSocketClient _client;
+        CommandHandler _handler;
+
         static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult(); //forces the program to run StartAsync instead of the main
+
+        public async Task StartAsync()
         {
+            if (Config.bot.token == "" || Config.bot.token == null) return; //ensures the token is real and exists and breaks if it isn't
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+            _client.Log += Log;
+            
+            await _client.LoginAsync(TokenType.Bot, Config.bot.token);
+            await _client.StartAsync();
+            _handler = new CommandHandler();
+            await _handler.InitializeAsync(_client);
+            await Task.Delay(-1);
+        }
+
+        private async Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.Message);
         }
     }
 }
